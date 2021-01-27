b08dc1a370f98eff31548685af16bf0d23b75275
Alisson Oliveira Silva (NEC - BR/Sao Paulo) <alissonos@nec.br>
2021-01-27 16:01:15 -0300

    Adicionando arquivo bin

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -13,6 +13,6 @@ $buscador = new Buscador($client, $crawler);
 $cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
-    echo exibeMensagem($curso);
+    exibeMensagem($curso);
 }
 
